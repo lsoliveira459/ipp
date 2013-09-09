9527b7b5a792ad7a9f9c9e89177b4a63fcf9d0c5
Lucas Oliveira <luk51000@gmail.com>
2013-09-09 11:51:05 -0300

    concluindo rel 04

--- exercises/c04/exp2a/serial.c
+++ exercises/c04/exp2a/serial.c
@@ -1,25 +1,14 @@
-/* serial.c -- serial trapezoidal rule
- *
- * Calculate definite integral using trapezoidal rule.
- * The function f(x) is hardwired.
- * Input: a, b, n.
- * Output: estimate of integral from a to b of f(x)
- *    using n trapezoids.
- *
- * See Chapter 4, pp. 53 & ff. in PPMPI.
- */
-
 #include <stdio.h>
 
 main() {
-    float  integral;   /* Store result in integral   */
-    float  a, b;       /* Left and right endpoints   */
-    int    n;          /* Number of trapezoids       */
-    float  h;          /* Trapezoid base width       */
+    float  integral;
+    float  a, b;
+    int    n;
+    float  h;
     float  x;
     int    i;
 
-    float f(float x);  /* Function we're integrating */
+    float f(float x);
 
     printf("Enter a, b, and n\n");
     scanf("%f %f %d", &a, &b, &n);
@@ -37,11 +26,8 @@ main() {
     printf("of the integral from %f to %f = %f\n", a, b, integral);
 }
 
-
 float f(float x) {
     float return_val;
-    /* Calculate f(x).  Store calculation in return_val. */
-
     return_val = x*x;
     return return_val;
-} /* f */
+}
--- exercises/c04/exp2b/get_data.c
+++ exercises/c04/exp2b/get_data.c
@@ -2,29 +2,26 @@
 #include "mpi.h"
 
 main(int argc, char** argv) {
-    int         my_rank;   /* My process rank           */
-    int         p;         /* The number of processes   */
-    float       a;         /* Left endpoint             */
-    float       b;         /* Right endpoint            */
-    int         n;         /* Number of trapezoids      */
-    int         f;         /* Function identifier       */
-    float       h;         /* Trapezoid base length     */
-    float       local_a;   /* Left endpoint my process  */
-    float       local_b;   /* Right endpoint my process */
-    int         local_n;   /* Number of trapezoids for  */
-                           /* my calculation            */
-    float     (*local_f)(float); /* Pointer to function */
-    float       integral;  /* Integral over my interval */
-    float       total;     /* Total integral            */
-    int         source;    /* Process sending integral  */
-    int         dest = 0;  /* All messages go to 0      */
+    int         my_rank;
+    int         p;
+    float       a;
+    float       b;
+    int         n;
+    int         f;
+    float       h;
+    float       local_a;
+    float       local_b;
+    int         local_n;
+    float     (*local_f)(float);
+    float       integral;
+    float       total;
+    int         source;
+    int         dest = 0;
     int         tag = 0;
     MPI_Status  status;
 
-    void Get_data(float* a_ptr, float* b_ptr, 
-         int* n_ptr, int* f_ptr, int my_rank, int p);
-    float Trap(float local_a, float local_b, int local_n, float (*f)(float),
-              float h);    /* Calculate local integral  */
+    void Get_data(float* a_ptr, float* b_ptr, int* n_ptr, int* f_ptr, int my_rank, int p);
+    float Trap(float local_a, float local_b, int local_n, float (*f)(float), float h);
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
@@ -43,8 +40,8 @@ main(int argc, char** argv) {
        default: local_f = m;break;
     }
 
-    h = (b-a)/n;    /* h is the same for all processes */
-    local_n = n/p;  /* So is the number of trapezoids */
+    h = (b-a)/n;
+    local_n = n/p;
 
     local_a = a + my_rank*local_n*h;
     local_b = local_a + local_n*h;
@@ -69,15 +66,15 @@ main(int argc, char** argv) {
 }
 
 void Get_data(
-         float*  a_ptr    /* out */, 
-         float*  b_ptr    /* out */, 
-         int*    n_ptr    /* out */,
-         int*    f_ptr    /* out */,
-         int     my_rank  /* in  */, 
-         int     p        /* in  */) {
+         float*  a_ptr, 
+         float*  b_ptr, 
+         int*    n_ptr,
+         int*    f_ptr,
+         int     my_rank, 
+         int     p) {
 
-    int source = 0;    /* All local variables used by */
-    int dest;          /* MPI_Send and MPI_Recv       */
+    int source = 0;
+    int dest;
     int tag;
     MPI_Status status;
 
@@ -104,19 +101,17 @@ void Get_data(
         tag = 3;
         MPI_Recv(f_ptr, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
     }
-} /* Get_data */
+}
 
-
-/********************************************************************/
 float Trap(
-          float  local_a   /* in */, 
-          float  local_b   /* in */, 
-          int    local_n   /* in */,
-          float  (*local_f)(float)/* in */,
-          float  h         /* in */
+          float  local_a, 
+          float  local_b, 
+          int    local_n,
+          float  (*local_f)(float),
+          float  h
         ) { 
 
-    float integral;   /* Store result in integral  */ 
+    float integral;
     float x; 
     int i;
 
@@ -128,30 +123,19 @@ float Trap(
     } 
     integral = integral*h/3.0; 
     return integral;
-} /*  Trap  */
-
-
-/********************************************************************/
+}
 float k(float x) { 
-    float return_val; 
-    /* Calculate f(x). */
-    /* Store calculation in return_val. */ 
+    float return_val;
     return_val = x*x;
     return return_val; 
-} /* k */
+}
 float l(float x) { 
     float return_val; 
-    /* Calculate f(x). */
-    /* Store calculation in return_val. */ 
     return_val = x*x+1;
     return return_val; 
-} /* l */
+}
 float m(float x) { 
     float return_val; 
-    /* Calculate f(x). */
-    /* Store calculation in return_val. */ 
     return_val = x*x+2;
     return return_val; 
-} /* m */
-
-
+}
